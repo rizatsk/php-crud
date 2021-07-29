b42713374327783109e761e5dc3c48da1fc3bc99
rizatsk <rizatsakmir@gmail.com>
2021-07-29 15:09:38 +0700

    merubah isi dari file index.php

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ $mahasiswa = read("SELECT * FROM tb_mahasiswa");
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="">Home</a>
+              <a class="nav-link active" aria-current="page" href="">Data Mahasiswa</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="create.php">CREATE DATA</a>
